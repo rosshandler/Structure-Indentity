ca4f9f018be2bb6e8f523eaee964bea67cec9192
rosshandler <ivanir@mrc-lmb.cam.ac.uk>
2021-11-29 10:51:03 +0000

    Update miscellaneous.R

--- snRNA-seq/miscellaneous.R
+++ snRNA-seq/miscellaneous.R
@@ -1,1 +1,74 @@
 
+## Colours
+
+leiden_colours <- c(
+  "0"="#cb459b",
+  "1"="#5db645",
+  "2"="#c267d3",
+  "3"="#a6b547",
+  "4"="#6d64d5",
+  "5"="#d39c3f",
+  "6"="#6e83cc",
+  "7"="#d25337",
+  "8"="#4aadd6",
+  "9"="#dc446c",
+  "10"="#5bbe7d",
+  "11"="#9761aa",
+  "12"="#3a7f4b",
+  "13"="#e088aa",
+  "14"="#50bba7",
+  "15"="#a24a5f",
+  "16"="#747a32",
+  "17"="#b77345",
+  "18"="#3c4dbb",
+  "19"="#9e4d70",
+  "20"="#bbb633",
+  "21"="#df8172",
+  "22"="#677328",
+  "23"="#a6542e",
+  "24"="#91702c"
+)
+
+sample_colours <- c(
+  "B6Kit"="#787ec7",
+  "B7Kit"="#64ac48",
+  "B8Kit"="#7062d0",
+  "B6Fast"="#9a963f",
+  "B7Fast"="#be5dc2",
+  "B8Fast"="#4aaa86",
+  "B6diss"="#d13d7e",
+  "B7diss"="#4facd8",
+  "B8diss"="#d04f33",
+  "B6Unembed"="#be6c9b",
+  "B8Unembed_A"="#c88742",
+  "B8Unembed_B"="#c2605f"
+)
+
+condition_colours <- c(
+  "Kit"="#e12f2f",
+  "Fast"="#6889ff",
+  "Diss"="#9a9100",
+  "Unembed"="#00733a"
+)
+
+population_colours_manuscript <- c(
+  "Mitotic RG" = "#005dd8",
+  "Cycling RG" = "#4aadd6",
+  "Differentiating RG" = "#6083d1",
+  "Apical RG" = "#02d7ec",
+  "Glycolytic RG" = "#0086cb",
+  "High trancription RG" = "#4aac8d",
+  "Chp" = "#935de6",
+  "Cortical hem" = "#9e4d70",
+  "Cycling IPCs" = "#d74897",
+  "IPCs"   = "#ff8ba6",
+  #"High biosynthesis"   = "#ff6500",
+  "IN"="#228B22",
+  "Mature IN" = "#90b600",
+  "CR cells" = "#00ff00",
+  "Migrating excitatory neurons" = "#a6023e",
+  "UL neurons"   = "#fa2274",
+  "DL neurons"   = "#9e5d56",
+  "Mature excitatory neurons"="#d3b000"
+)
+
