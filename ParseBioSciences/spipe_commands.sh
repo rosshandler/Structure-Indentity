cef95ceaaf9f17249eddd272dd4bda9014ce6447
rosshandler <ivanir@mrc-lmb.cam.ac.uk>
2023-01-18 16:21:52 +0000

    Update spipe_commands.sh

--- ParseBioSciences/spipe_commands.sh
+++ ParseBioSciences/spipe_commands.sh
@@ -1,18 +1,25 @@
-# spipe is already installed as conda env
-conda activate spipe
+# spipe.v1.0.0 is already installed as conda env
+conda activate spipe.v1.0.0
 
+PBS='/data1/ivanir/Ilaria2023/ParseBS'
 
-PBS='/data1/ivanir/Chp2022/ParseBS'
+PATH="/data1/ivanir/Ilaria2023/ParseBS/ParseBiosciences-Pipeline.1.0.3p:$PATH"
 
-# Genome indexing --> see Chp repository
+# Genome indexing
+split-pipe \
+--mode mkref \
+--genome_name hg38 \
+--fasta $PBS/newvolume/genomes/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz \
+--genes $PBS/newvolume/genomes/Homo_sapiens.GRCh38.108.gtf.gz \
+--output_dir $PBS/newvolume/genomes/hg38
 
 # Pipeline running 
-split-pipe --mode all --kit WT_mini --genome_dir $PBS/newvolume/genomes/hg38/ \
---fq1 $PBS/newvolume/expdata/SLX-21921.DNAA007.H52HVDRX2.s_2.r_1.fq.gz \
---fq2 $PBS/newvolume/expdata/SLX-21921.DNAA007.H52HVDRX2.s_2.r_2.fq.gz \
---output_dir $PBS/newvolume/analysis/DNAA008 
+split-pipe --mode all --kit WT --chemistry v2 --genome_dir $PBS/newvolume/genomes/hg38/ \
+--fq1 $PBS/newvolume/expdata/SLX-22601.UnspecifiedIndex.HGTNNDMXY.s_1.r_1.fq.gz \
+--fq2 $PBS/newvolume/expdata/SLX-22601.UnspecifiedIndex.HGTNNDMXY.s_1.r_2.fq.gz \
+--output_dir $PBS/newvolume/analysis/S1 
 
-split-pipe --mode all --kit WT_mini --genome_dir $PBS/newvolume/genomes/hg38/ \
---fq1 $PBS/newvolume/expdata/SLX-21921.DNAA008.H52HVDRX2.s_2.r_1.fq.gz \
---fq2 $PBS/newvolume/expdata/SLX-21921.DNAA008.H52HVDRX2.s_2.r_2.fq.gz \
---output_dir $PBS/newvolume/analysis/DNAA008 
+split-pipe --mode all --kit WT --chemistry v2 --genome_dir $PBS/newvolume/genomes/hg38/ \
+--fq1 $PBS/newvolume/expdata/SLX-22601.UnspecifiedIndex.HGTNNDMXY.s_2.r_1.fq.gz \
+--fq2 $PBS/newvolume/expdata/SLX-22601.UnspecifiedIndex.HGTNNDMXY.s_2.r_2.fq.gz \
+--output_dir $PBS/newvolume/analysis/S2 
