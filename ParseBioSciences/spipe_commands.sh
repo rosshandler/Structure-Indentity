32dc6e190ed4a2250805339eca7aa5d76ef83382
rosshandler <ivanir@mrc-lmb.cam.ac.uk>
2023-01-20 12:48:43 +0000

    Update spipe_commands.sh

--- ParseBioSciences/spipe_commands.sh
+++ ParseBioSciences/spipe_commands.sh
@@ -13,28 +13,27 @@ split-pipe \
 --genes $PBS/newvolume/genomes/Homo_sapiens.GRCh38.108.gtf.gz \
 --output_dir $PBS/newvolume/genomes/hg38
 
-# Pipeline running 
-split-pipe --mode all --kit WT --chemistry v2 --genome_dir $PBS/newvolume/genomes/hg38/ \
---fq1 $PBS/newvolume/expdata/SLX-22601.UnspecifiedIndex.HGTNNDMXY.s_1.r_1.fq.gz \
---fq2 $PBS/newvolume/expdata/SLX-22601.UnspecifiedIndex.HGTNNDMXY.s_1.r_2.fq.gz \
---output_dir $PBS/newvolume/analysis/S1 
+# Merge both lanes of single cell experiment
+cd $PBS/newvolume/expdata/
+cat SLX-22601.UnspecifiedIndex.HGTNNDMXY.s_1.r_1.fq.gz SLX-22601.UnspecifiedIndex.HGTNNDMXY.s_2.r_2.fq.gz > SLX-22601.r_1.fq.gz
+cat SLX-22601.UnspecifiedIndex.HGTNNDMXY.s_1.r_1.fq.gz SLX-22601.UnspecifiedIndex.HGTNNDMXY.s_2.r_2.fq.gz > SLX-22601.r_2.fq.gz
 
-split-pipe --mode all --kit WT --chemistry v2 --genome_dir $PBS/newvolume/genomes/hg38/ \
---fq1 $PBS/newvolume/expdata/SLX-22601.UnspecifiedIndex.HGTNNDMXY.s_2.r_1.fq.gz \
---fq2 $PBS/newvolume/expdata/SLX-22601.UnspecifiedIndex.HGTNNDMXY.s_2.r_2.fq.gz \
---output_dir $PBS/newvolume/analysis/S2 
-
+# Pipeline running
+#single cell
 split-pipe --mode all --kit WT --chemistry v2 --genome_dir $PBS/newvolume/genomes/hg38/ \
 --fq1 $PBS/newvolume/expdata/SLX-22601.r_1.fq.gz \
 --fq2 $PBS/newvolume/expdata/SLX-22601.r_2.fq.gz \
 --output_dir $PBS/newvolume/analysis/sCell 
 
+#single nuclei
 split-pipe --mode all --kit WT_mini --chemistry v2 --genome_dir $PBS/newvolume/genomes/hg38/ \
 --fq1 $PBS/newvolume/expdata/SLX-22604.UnspecifiedIndex.HNLNNDRX2.s_2.r_1.fq.gz \
 --fq2 $PBS/newvolume/expdata/SLX-22604.UnspecifiedIndex.HNLNNDRX2.s_2.r_2.fq.gz \
---output_dir $PBS/newvolume/analysis/nuclei 
+--output_dir $PBS/newvolume/analysis/sNuclei 
 
 
+
+...
 # Pipeline running 
 split-pipe --mode dge --tscp_use 500 --kit WT --chemistry v2 --genome_dir $PBS/newvolume/genomes/hg38/ \
 --fq1 $PBS/newvolume/expdata/SLX-22601.UnspecifiedIndex.HGTNNDMXY.s_1.r_1.fq.gz \
