866e911a90efc56de6dfdc51cdf6fa4d1776aae9
rosshandler <ivanir@mrc-lmb.cam.ac.uk>
2023-02-15 10:54:04 +0000

    Update spipe_commands.sh

--- ParseBioSciences/spipe_commands.sh
+++ ParseBioSciences/spipe_commands.sh
@@ -45,42 +45,42 @@ rm SLX-22601.r_1.fq.gz SLX-22601.r_2.fq.gz
 split-pipe --mode all --kit WT --chemistry v2 --genome_dir $PBS/newvolume/genomes/hg38/ \
 --fq1 $PBS/newvolume/expdata/correctFastq/SLX-22601.DNAA007.HGMLNDMXY.ACTTGA.s_1.r_1.fq.gz \
 --fq2 $PBS/newvolume/expdata/correctFastq/SLX-22601.DNAA007.HGMLNDMXY.ACTTGA.s_1.r_2.fq.gz \
---output_dir $PBS/newvolume/analysis/sCell
+--output_dir $PBS/newvolume/analysis/sCell/ACTTGA
 
 split-pipe --mode all --kit WT --chemistry v2 --genome_dir $PBS/newvolume/genomes/hg38/ \
 --fq1 $PBS/newvolume/expdata/correctFastq/SLX-22601.DNAA007.HGMLNDMXY.AGTCAA.s_1.r_1.fq.gz \
 --fq2 $PBS/newvolume/expdata/correctFastq/SLX-22601.DNAA007.HGMLNDMXY.AGTCAA.s_1.r_2.fq.gz \
---output_dir $PBS/newvolume/analysis/sCell
+--output_dir $PBS/newvolume/analysis/sCell/AGTCAA
 
 split-pipe --mode all --kit WT --chemistry v2 --genome_dir $PBS/newvolume/genomes/hg38/ \
 --fq1 $PBS/newvolume/expdata/correctFastq/SLX-22601.DNAA007.HGMLNDMXY.AGTTCC.s_1.r_1.fq.gz \
 --fq2 $PBS/newvolume/expdata/correctFastq/SLX-22601.DNAA007.HGMLNDMXY.AGTTCC.s_1.r_2.fq.gz \
---output_dir $PBS/newvolume/analysis/sCell
+--output_dir $PBS/newvolume/analysis/sCell/AGTTCC
 
 split-pipe --mode all --kit WT --chemistry v2 --genome_dir $PBS/newvolume/genomes/hg38/ \
 --fq1 $PBS/newvolume/expdata/correctFastq/SLX-22601.DNAA007.HGMLNDMXY.ATGTCA.s_1.r_1.fq.gz \
 --fq2 $PBS/newvolume/expdata/correctFastq/SLX-22601.DNAA007.HGMLNDMXY.ATGTCA.s_1.r_2.fq.gz \
---output_dir $PBS/newvolume/analysis/sCell
+--output_dir $PBS/newvolume/analysis/sCell/ATGTCA
 
 split-pipe --mode all --kit WT --chemistry v2 --genome_dir $PBS/newvolume/genomes/hg38/ \
 --fq1 $PBS/newvolume/expdata/correctFastq/SLX-22601.DNAA007.HGMLNDMXY.CAGATC.s_1.r_1.fq.gz \
 --fq2 $PBS/newvolume/expdata/correctFastq/SLX-22601.DNAA007.HGMLNDMXY.CAGATC.s_1.r_2.fq.gz \
---output_dir $PBS/newvolume/analysis/sCell
+--output_dir $PBS/newvolume/analysis/sCell/CAGATC
 
 split-pipe --mode all --kit WT --chemistry v2 --genome_dir $PBS/newvolume/genomes/hg38/ \
 --fq1 $PBS/newvolume/expdata/correctFastq/SLX-22601.DNAA007.HGMLNDMXY.CTTGTA.s_1.r_1.fq.gz \
 --fq2 $PBS/newvolume/expdata/correctFastq/SLX-22601.DNAA007.HGMLNDMXY.CTTGTA.s_1.r_2.fq.gz \
---output_dir $PBS/newvolume/analysis/sCell
+--output_dir $PBS/newvolume/analysis/sCell/CTTGTA
 
 split-pipe --mode all --kit WT --chemistry v2 --genome_dir $PBS/newvolume/genomes/hg38/ \
 --fq1 $PBS/newvolume/expdata/correctFastq/SLX-22601.DNAA007.HGMLNDMXY.GATCAG.s_1.r_1.fq.gz \
 --fq2 $PBS/newvolume/expdata/correctFastq/SLX-22601.DNAA007.HGMLNDMXY.GATCAG.s_1.r_2.fq.gz \
---output_dir $PBS/newvolume/analysis/sCell
+--output_dir $PBS/newvolume/analysis/sCell/GATCAG
 
 split-pipe --mode all --kit WT --chemistry v2 --genome_dir $PBS/newvolume/genomes/hg38/ \
 --fq1 $PBS/newvolume/expdata/correctFastq/SLX-22601.DNAA007.HGMLNDMXY.TAGCTT.s_1.r_1.fq.gz \
 --fq2 $PBS/newvolume/expdata/correctFastq/SLX-22601.DNAA007.HGMLNDMXY.TAGCTT.s_1.r_2.fq.gz \
---output_dir $PBS/newvolume/analysis/sCell
+--output_dir $PBS/newvolume/analysis/sCell/TAGCTT
 
 #single nuclei
 nohup /home/ivanir/tools/demultiplexer.rhel/demultiplexer.rhel/demuxFQ \
