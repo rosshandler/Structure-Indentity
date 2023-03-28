47f18866e39ee2f2b18990f2cb2b17bd87ddfd4a
rosshandler <17701395+rosshandler@users.noreply.github.com>
2023-03-28 16:51:26 -0700

    Create findMarkers_annotation.R

---
+++ ParseBioSciences/scRNAseq/findMarkers_annotation.R
@@ -0,0 +1,13 @@
+library(scran)
+
+setwd('/data1/ivanir/Ilaria2023/ParseBS/newvolume/analysis/sCell/combined/celltype_annotation/')
+load('plots_Ilaria_PB_28March2023.RData')
+
+markers <- findMarkers(sce_ctrl, groups = df_plot$leiden, direction = "up")
+
+for (clus in as.character(1:17)){
+  markers_clus <- markers[[clus]]
+  markers_clus <- cbind(symbol=rownames(markers_clus), markers_clus)
+  write.table(markers_clus, file = paste0("DiffExp_Cluster",clus,".txt"), row.names = FALSE, sep = "\t", quote = FALSE)
+  print(clus)
+}
