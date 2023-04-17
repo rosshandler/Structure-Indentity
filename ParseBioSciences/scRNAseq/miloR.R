7dffa84e5e56b45f9270d718c2f0d55a8c0c3ae5
rosshandler <17701395+rosshandler@users.noreply.github.com>
2023-04-16 19:49:10 -0700

    Update miloR.R

--- ParseBioSciences/scRNAseq/miloR.R
+++ ParseBioSciences/scRNAseq/miloR.R
@@ -1,97 +1,94 @@
 library(miloR)
 library(scran)
+library(irlba)
 library(scater)
 library(Matrix)
 library(ggplot2)
 
 library(patchwork)
 
-setwd("/data1/ivanir/Ilaria2021/data")
+path2data   <- '/data1/ivanir/Ilaria2023/ParseBS/newvolume/analysis/sCell/combined/all-well/DGE_unfiltered/'
+sce <- readRDS(paste0(path2data,"sce.rds"))
+sce <- sce[,colData(sce)$doublet_class == "singlet"]
 
-layout_umap <- read.csv("umap_layout_batch_corrected_contamination_cleaned.csv", header = TRUE)
+setwd("/data1/ivanir/Ilaria2023/ParseBS/newvolume/analysis/sCell/combined/sctour")
 
-meta_clus   <- read.csv("meta_annotated_updated.csv", header = TRUE)
+umap <- read.csv("umap_layout.csv", header = FALSE)
+colnames(umap) <- c("UMAP1","UMAP2")
+rownames(umap) <- colnames(sce)
+meta <- read.csv("metadata_scanpy.csv", header = TRUE)[,-1]
 
-sce <- readRDS("sce.rds")
-sce <- sce[calculateAverage(sce)>0.05, colData(sce)$scDblFinder.class == "singlet"]
-sce <- logNormCounts(sce)
+sce_filt <- sce[calculateAverage(sce)>0.01,]
+sce_filt <- logNormCounts(sce_filt)
 
-pca <- read.csv("pca_corrected_seurat.csv")
-rownames(pca) <- colnames(sce)
+decomp  <- modelGeneVar(sce_filt)
+hvgs    <- rownames(decomp)[decomp$FDR < 0.1]
+length(hvgs)
+#[1] 783
 
-sce  <- sce[,meta_clus$celltype_annotation != "High metabolism/protein translation"]
-pca  <- pca[meta_clus$celltype_annotation != "High metabolism/protein translation",]
-meta <- meta_clus[meta_clus$celltype_annotation != "High metabolism/protein translation",]
+pca <- prcomp_irlba(t(logcounts(sce_filt[hvgs,])), n = 30)
+rownames(pca$x) <- colnames(sce_filt)
+reducedDim(sce_filt, "PCA") <- as.matrix(pca$x)
 
-pca_morph <- readRDS("morphology_pca.rds")
-rownames(pca_morph$x) <- c(
-  "B6Kit", "B7Kit", "B8Kit",
-  "B6Fast", "B7Fast", "B8Fast",
-  "B6Unembed", "B8Unembed_A", "B8Unembed_B",
-  "B6diss", "B7diss", "B8diss")
+setwd("/data1/ivanir/Ilaria2023/ParseBS/newvolume/analysis/sCell/combined/miloR")
 
-set.seed(14)
-clustering <- kmeans(pca_morph$x[,1:2], centers = 2, nstart = 25) # same result if three PCs are used
-
-pdf("kmeans2_morphology.pdf")
-fviz_cluster(clustering, data = pca$x[,1:2],
-             palette = c("#2E9FDF", "#E7B800"), 
-             geom = "point",
-             ellipse.type = "convex", 
-             ggtheme = theme_bw()
-)
-dev.off()
-
-meta$morphology_clus <- clustering$cluster[as.character(meta$sample)]
-
-meta <- data.frame(
- meta,
- UMAP1 = layout_umap[,1],
- UMAP2 = layout_umap[,2] 
-)
-
-setwd("/data1/ivanir/Ilaria2021/data/milo_plots")
-
-reducedDim(sce, "PCA") <- as.matrix(pca)
-reducedDims(sce)
-
-milo.obj <- Milo(sce)
+milo.obj <- Milo(sce_filt)
 
 milo.obj <- buildGraph(milo.obj, k=20, d=30)
 milo.obj <- makeNhoods(milo.obj, k=20, d=30, refined=TRUE, prop=0.2)
-
-pdf("nhood_size_hist.pdf")
 plotNhoodSizeHist(milo.obj)
-dev.off()
 
 milo.obj <- calcNhoodDistance(milo.obj, d=30)
-milo.obj <- countCells(milo.obj, samples="sample", meta.data=meta)
+milo.obj <- countCells(milo.obj, samples="sample_name", meta.data=meta)
 
-milo.design <- as.data.frame(xtabs(~ condition + sample, data=meta))
+reducedDim(milo.obj, "UMAP") <- as.matrix(umap) 
+saveRDS(milo.obj,"MiloRscRNASeqObject.rds")
+
+milo.design <- as.data.frame(xtabs(~ condition + sample_name, data=meta))
 milo.design <- milo.design[milo.design$Freq > 0, ]
-rownames(milo.design) <- milo.design$sample
+rownames(milo.design) <- milo.design$sample_name
+
+table(meta$condition)
+#CTRL_45 CTRL_55 CTRL_70 DISS_48 DISS_55 DISS_70   
+#   7274    7291    2042    1460    8824    7312    4875
+
+milo.res0 <- testNhoods(milo.obj, design=~condition+0, design.df=milo.design,
+  model.contrasts="conditionCTRL_55 - conditionCTRL_45")
+head(milo.res0[order(milo.res0$PValue, decreasing=FALSE),])
 
 milo.res1 <- testNhoods(milo.obj, design=~condition+0, design.df=milo.design,
-  model.contrasts="conditionFast - conditionKit")
-head(milo.res1[order(milo.res1$PValue, decreasing=FALSE),])
+  model.contrasts="conditionDISS_48 - conditionCTRL_45")
 
 milo.res2 <- testNhoods(milo.obj, design=~condition+0, design.df=milo.design,
-  model.contrasts="conditionUnembed - conditionKit")
-head(milo.res2[order(milo.res2$PValue, decreasing=FALSE),])
+  model.contrasts="conditionDISS_48 - conditionCTRL_55")
 
 milo.res3 <- testNhoods(milo.obj, design=~condition+0, design.df=milo.design,
-  model.contrasts="conditionDiss - conditionKit")
-head(milo.res3[order(milo.res3$PValue, decreasing=FALSE),])
+  model.contrasts="conditionDISS_55 - conditionCTRL_45")
+
+milo.res4 <- testNhoods(milo.obj, design=~condition+0, design.df=milo.design,
+  model.contrasts="conditionDISS_55 - conditionCTRL_55")
+
+milo.res5 <- testNhoods(milo.obj, design=~condition+0, design.df=milo.design,
+  model.contrasts="conditionEMB_55 - conditionCTRL_55")
+
+milo.res6 <- testNhoods(milo.obj, design=~condition+0, design.df=,
+  model.contrasts="conditionDISS_48 - conditionDISS_55")
+
+milo.res7 <- testNhoods(milo.obj, design=~condition+0, design.df=milo.design,
+  model.contrasts="conditionCTRL_70 - conditionCTRL_55")
+
+milo.res8 <- testNhoods(milo.obj, design=~condition+0, design.df=milo.design,
+  model.contrasts="conditionDISS_70 - conditionDISS_48")
+
+milo.res9 <- testNhoods(milo.obj, design=~condition+0, design.df=milo.design,
+  model.contrasts="conditionDISS_70 - conditionDISS_55")
+
+milo.res10 <- testNhoods(milo.obj, design=~condition+0, design.df=milo.design,
+  model.contrasts="conditionDISS_70 - conditionCTRL_70")
 
 milo.obj <- buildNhoodGraph(milo.obj)
 
-layout <- meta[, c("UMAP1","UMAP2")]
-rownames(layout) <- colnames(sce)
-
-reducedDim(milo.obj, "UMAP") <- as.matrix(layout) 
-
-pdf("milo_noMG.pdf",width=12,height=6)
-#plotUMAP(milo.obj) + plotNhoodGraphDA(milo.obj, milo.res2, alpha=0.05) +
+pdf("milo0.pdf",width=12,height=6)
 plotNhoodGraphDA(milo.obj, milo.res2, alpha=0.5) +
   plot_layout(guides="collect")
 dev.off()
