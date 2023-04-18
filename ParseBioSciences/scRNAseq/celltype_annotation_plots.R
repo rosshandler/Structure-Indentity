dec147f38874b938514f83167af9d283e1b84448
rosshandler <17701395+rosshandler@users.noreply.github.com>
2023-04-17 20:48:26 -0700

    Update celltype_annotation_plots.R

--- ParseBioSciences/scRNAseq/celltype_annotation_plots.R
+++ ParseBioSciences/scRNAseq/celltype_annotation_plots.R
@@ -5,11 +5,6 @@ library(Matrix)
 library(ggplot2)
 
 
-library(umap)
-library(leiden)
-library(reticulate)
-use_condaenv(condaenv="scanpy-p3.9")
-
 population_colours <- c(
 "Mitotic RG" = "#005dd8",
 "Cycling RG" = "#4aadd6",
@@ -30,20 +25,24 @@ population_colours <- c(
 "Mature excitatory neurons"="#d3b000")
 
 leiden_colours <- c(
-"#ff9388",
-"#2272fc",
-"#ffb546",
-"#732db2",
-"#a5d471",
-"#ff7dda",
-"#006d24",
-"#8e2f5c",
-"#1fdfba",
-"#8f3617",
-"#00c6e9",
-"#7a7700",
-"#7d4932")
-names(cluster_colours)<-1:14
+"#da4b84",
+"#6bb442",
+"#ad5cd0",
+"#b8ae48",
+"#6861c8",
+"#dc9848",
+"#6582c7",
+"#d35a2c",
+"#48b6d2",
+"#cb4046",
+"#54ad7b",
+"#c24aa6",
+"#697730",
+"#c788ca",
+"#a16436",
+"#9d486a",
+"#df8081")
+names(cluster_colours)<-1:17
 
 day_colours <- c(
 "#0054b9",
@@ -55,33 +54,6 @@ sce  <- readRDS("/data1/ivanir/Ilaria2023/ParseBS/newvolume/analysis/sCell/combi
 sce  <- logNormCounts(sce)
 gexp <- logcounts(sce)
 df_plot <- data.frame(colData(sce), reducedDim(sce, "UMAP"))
-
-sce_hvgs <- sce[calculateAverage(sce)>0.01,]
-decomp   <- modelGeneVar(sce_hvgs)
-hvgs     <- rownames(decomp)[decomp$FDR < 0.0005]
-writeLines(hvgs,"/data1/ivanir/Ilaria2023/ParseBS/newvolume/analysis/sCell/combined/all-well/DGE_unfiltered/scranHVGsFDR.01.txt")
-markers <- readLines("/data1/ivanir/Ilaria2023/ParseBS/newvolume/analysis/sCell/combined/all-well/DGE_unfiltered/geneMarkers.txt")
-markers <- intersect(markers, rownames(sce))
-hvgs    <- union(hvgs, markers)
-pca     <- prcomp_irlba(t(logcounts(sce[hvgs,])), n = 30)
-rownames(pca$x) <- colnames(sce)
-layout  <- umap(pca$x, method="umap-learn", umap_learn_args=c("n_neighbors", "n_epochs", "min_dist"), n_neighbors=30, min_dist=.25)
-
-graph <- buildSNNGraph(pca$x, d = NA, transposed = TRUE)
-set.seed(42)
-clusters <- leiden(graph, resolution_parameter = 2)
-names(clusters) <-  colData(sce)$cell
-
-df_plot <- data.frame(
- colData(sce),
- leiden = clusters,
- UMAP1 = layout$layout[,1],
- UMAP2 = layout$layout[,2] 
-)
-
-writeMM(t(logcounts(sce)), "/data1/ivanir/Ilaria2023/ParseBS/newvolume/analysis/sCell/combined/sctour/norm_counts_hvgs.mtx", sep="\t")
-writeLines(colnames(sce), "/data1/ivanir/Ilaria2023/ParseBS/newvolume/analysis/sCell/combined/sctour/cells_norm_hvgs.txt")
-writeLines(rowData(sce)$gene_name, "/data1/ivanir/Ilaria2023/ParseBS/newvolume/analysis/sCell/combined/sctour/genes_norm_hvgs.txt")
 
 plotLayoutExpression <- function(gene="TTR"){
   require(Matrix)
