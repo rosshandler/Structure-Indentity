cada6bd19f84502b58f31007f1b8f53dca6f7444
rosshandler <17701395+rosshandler@users.noreply.github.com>
2023-04-24 11:23:55 -0700

    Update celltype_annotation_cluster_markers.R

--- ParseBioSciences/scRNAseq/celltype_annotation_cluster_markers.R
+++ ParseBioSciences/scRNAseq/celltype_annotation_cluster_markers.R
@@ -5,7 +5,7 @@ load('plots_Ilaria_PB_24Apr2023.RData')
 
 markers <- findMarkers(sce, groups = df_plot$leiden, direction = "up")
 
-for (clus in as.character(1:17)){
+for (clus in as.character(0:20)){
   markers_clus <- markers[[clus]]
   markers_clus <- cbind(symbol=rownames(markers_clus), markers_clus)
   write.table(markers_clus, file = paste0("DiffExp_Cluster",clus,".txt"), row.names = FALSE, sep = "\t", quote = FALSE)
